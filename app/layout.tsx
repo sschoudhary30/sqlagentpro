b6a185ed600b854a761f931cff40d565df0c8017
suresh <suresh12345x@gmail.com>
2025-10-23 01:19:22 +0530

    Favicon change

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "SQLAgent Pro",
   description: "SQLAgent Pro",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
